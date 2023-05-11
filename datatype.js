b977512068e50076b80c1874f8ef126628e209d0
Aman Bansal <bansalaman1991@gmail.com>
2023-05-11 19:49:10 +0530

    added datatype file for the first commit

---
+++ datatype.js
@@ -0,0 +1,24 @@
+//variables
+
+var country = "india";
+var name = "Aman"
+var age = 31;
+var eligibletovote = true;
+console.log("hi", name, "!How are You?")
+console.log(typeof age)
+console.log(typeof name)
+var friends = ['pranay', 'pratham', 'rohit']
+var arrayValues = ['india', 100, true , "orsnge"]
+console.log('Array Values:', arrayValues);
+console.log(typeof arrayValues);
+var myObject = {
+    name:"Aman",
+    email:"bansalaman1991@gmail.com",
+    mobileNo:8451974499
+};
+console.log(typeof myObject);
+console.log(friends[2]);
+console.log("length of array",friends.length);
+console.log(myObject.profession);
+console.log(myObject['email']);
+
